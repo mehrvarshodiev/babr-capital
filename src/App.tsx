b92165cdca87ebe375cfb6c232e7534e5b68d22f
Mehrvar Shodiev <mehrvarshodiev2406@gmail.com>
2026-08-29 03:24:35 +0500

    Integrate floating WhatsApp contact button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import WhatsAppButton from './components/WhatsAppButton/WhatsAppButton';
 
 export default function App() {
   useEffect(() => { document.documentElement.classList.add('scroll-smooth'); return () => document.documentElement.classList.remove('scroll-smooth'); }, []);
-  return <Routes><Route path="*" element={<Home />} /></Routes>;
+  return <><Routes><Route path="*" element={<Home />} /></Routes><WhatsAppButton /></>;
 }
