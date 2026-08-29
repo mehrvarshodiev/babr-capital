fdd4902aba1163c0a67fd6c7667a3993a47c94ce
Mehrvar Shodiev <mehrvarshodiev2406@gmail.com>
2026-08-29 10:10:49 +0500

    Apply service-card color to application form

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import './light-glass.css';
+import './application-card-match.css';
 
 createRoot(document.getElementById('root')!).render(<StrictMode><BrowserRouter><App /></BrowserRouter></StrictMode>);
